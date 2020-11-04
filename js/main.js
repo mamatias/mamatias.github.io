4e131b910395fde003d171f20a28d8eb147434fb
matias <matiastorresrisso@gmail.com>
2020-11-04 19:03:23 -0300

    input version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+// Helper for new lines on the history side...
+function newLine(text) {
+    var line = document.createElement('div');
+    line.className = 'console-line';
+    line.innerHTML = text;
+    document.getElementById('history').appendChild(line);
+}
+
+// Helper for positioning caret at the end...
+function placeCaretAtEnd(el) {
+    el.focus();
+    if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+    }
+}
+
 // Structure creation
 window.onload = function() {
     // First, the wrapper
@@ -6,25 +32,74 @@ window.onload = function() {
     console1.id = 'wrapper';
     document.body.appendChild(console1);
 
+    // Historic cmds
     var console2 = document.createElement('div');
     console2.className = 'console';
     console2.id = 'history';
     document.getElementById('wrapper').appendChild(console2);
 
-    var console3 = document.createElement('div');
-    console3.className = 'console line';
-    console3.id = 'live';
-    document.getElementById('wrapper').appendChild(console3);
+    // Input part
+    var console2 = document.createElement('div');
+    console2.className = 'console';
+    console2.id = 'inputtxt';
+    //console2.type = 'text';
+    console2.contentEditable = true;
+    //console2.size = 5;
+    console2.spellcheck = false;
+    console2.innerText = 'root@trs>';
+    console2.addEventListener('input', inputListener);
+    console2.addEventListener('keypress', function(e){
+        if(e.key === 'Enter'){
+            introListener();
+        }
+    });
+    document.getElementById('wrapper').appendChild(console2);
 
-    var console4 = document.createElement('div');
-    console4.className = 'console';
-    console4.id = 'user';
-    console4.innerText = '>';
-    document.getElementById('live').appendChild(console4);
+    // Presentation
+    newLine('<b>Terminal Resume System</b> (@trs)<br>');
 
-    var console5 = document.createElement('input');
-    console5.className = 'console';
-    console5.id = 'inputtxt';
-    console5.type = 'text';
-    document.getElementById('live').appendChild(console5);
+    // Focus on the new line
+    document.getElementById('inputtxt').focus();
 };
+
+// Input listener
+function inputListener() {
+    var len = 30;
+    var c2 = document.getElementById('inputtxt');
+    itxt = c2.innerText;
+
+    // Erasing the \n key
+    if(itxt.search('\n') != -1){
+        itxt = 'xxx';
+    }
+
+    // A trick to avoid erasing initial part
+    if(itxt.length < 10){
+        itxt = 'root@trs>';
+    }
+
+    // Second part of the trick
+    if( itxt.substring(0,9) != 'root@trs>'){
+        itxt = 'root@trs>'+itxt;
+    }
+
+    // we need a 3rd part for avoid editing initial part.
+
+    // Avoiding inputs longer than <len>
+    if(itxt.length>len){
+        itxt = itxt.substring(0,len);
+    }
+    c2.innerText = itxt;
+    
+    placeCaretAtEnd(c2);
+}
+
+// ENTER or INTRO key listener
+function introListener(){
+    var c2 = document.getElementById('inputtxt');
+    itxt = c2.innerText;
+    c2.innerText = 'root@trs>';
+    placeCaretAtEnd(c2);
+    newLine('<i>'+itxt+'</i><br>');
+    console.log('enter...');
+}
