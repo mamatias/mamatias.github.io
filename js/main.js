8ae3a53a6074e2f06bdafff455d13e12c89196b5
matias <matiastorresrisso@gmail.com>
2020-11-08 11:42:34 -0300

    vPrevia a comandos

--- js/main.js
+++ js/main.js
@@ -28,7 +28,6 @@ function placeCaretAtEnd(el) {
 window.onload = function() {
     // First, the wrapper
     var console1 = document.createElement('div');
-    console1.className = 'console';
     console1.id = 'wrapper';
     document.body.appendChild(console1);
 
@@ -39,7 +38,7 @@ window.onload = function() {
     document.getElementById('wrapper').appendChild(console2);
 
     // Input part
-    var console2 = document.createElement('div');
+    var console2 = document.createElement('span');
     console2.className = 'console';
     console2.id = 'inputtxt';
     //console2.type = 'text';
@@ -50,6 +49,7 @@ window.onload = function() {
     console2.addEventListener('input', inputListener);
     console2.addEventListener('keypress', function(e){
         if(e.key === 'Enter'){
+            e.preventDefault();
             introListener();
         }
     });
@@ -66,40 +66,52 @@ window.onload = function() {
 function inputListener() {
     var len = 30;
     var c2 = document.getElementById('inputtxt');
+    var rerender = false;
     itxt = c2.innerText;
+    //chr1 = itxt.substring(itxt.length-2,itxt.length-1);
+    //chr2 = itxt.substring(itxt.length-1,itxt.length);
+    //console.log('['+chr1+'] -- <'+chr1.charCodeAt(0)+'>');
+    //console.log('['+chr2+'] -- <'+chr2.charCodeAt(0)+'>');
 
     // Erasing the \n key
     if(itxt.search('\n') != -1){
-        itxt = 'xxx';
-    }
-
-    // A trick to avoid erasing initial part
-    if(itxt.length < 10){
+        //itxt = 'root@trs>';
+        rerender = false;
+        console.log('bloqueado input...');
+    } else if(itxt.length < 10){
+        // A trick to avoid erasing initial part
         itxt = 'root@trs>';
-    }
+        rerender = true;
 
-    // Second part of the trick
-    if( itxt.substring(0,9) != 'root@trs>'){
+    } else if( itxt.substring(0,9) != 'root@trs>'){
+        // Second part of the trick
         itxt = 'root@trs>'+itxt;
-    }
+        rerender = true;
 
-    // we need a 3rd part for avoid editing initial part.
-
-    // Avoiding inputs longer than <len>
-    if(itxt.length>len){
+    } else if(itxt.length>len){
+        // Avoiding inputs longer than <len>
         itxt = itxt.substring(0,len);
+        rerender = true;
+
     }
-    c2.innerText = itxt;
+    // we need a 3rd part for avoid editing initial part.
     
-    placeCaretAtEnd(c2);
+
+    // Render again only if violated the pattern
+    if(rerender){
+        c2.innerText = itxt;
+        placeCaretAtEnd(c2);
+    }
 }
 
 // ENTER or INTRO key listener
 function introListener(){
     var c2 = document.getElementById('inputtxt');
+    var c3 = document.getElementById('wrapper');
     itxt = c2.innerText;
     c2.innerText = 'root@trs>';
     placeCaretAtEnd(c2);
     newLine('<i>'+itxt+'</i><br>');
     console.log('enter...');
+    c3.scrollBy(0,100);
 }
